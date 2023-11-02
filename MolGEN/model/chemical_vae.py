5641231caa7be2620b6ddb754648819e61997e52
fate1997 <rengp97@gmail.com>
2023-11-02 21:34:39 +0000

    initial complete ChemicalVAE

--- MolGEN/model/chemical_vae.py
+++ MolGEN/model/chemical_vae.py
@@ -1,10 +1,34 @@
 import torch
 from torch import nn
+from torch.nn import functional as F
 
 
 class ChemicalVAE(nn.Module):
-    def __init__(self, *args, **kwargs) -> None:
-        super().__init__(*args, **kwargs)
+    def __init__(self, 
+                 input_dim=120,
+                 vocab_size=35,
+                 latent_dim=292):
+        super().__init__()
+        
+        self.encoder = Encoder(input_dim, vocab_size, latent_dim)
+        self.decoder = Decoder(latent_dim, vocab_size, input_dim)
+    
+    def reparameterize(self, mu: torch.Tensor, logvar: torch.Tensor):
+        std = torch.exp(0.5 * logvar)
+        eps = torch.randn_like(std)
+        return eps.mul(std).add_(mu)
+
+    def forward(self, x: torch.Tensor):
+        mu, logvar = self.encoder(x)
+        z = self.reparameterize(mu, logvar)
+        return self.decoder(z), mu, logvar
+    
+    def loss_func(self, 
+                  x: torch.Tensor, x_hat: torch.Tensor, 
+                  mu: torch.Tensor, logvar: torch.Tensor):
+        reconstruction_loss = F.binary_cross_entropy(x_hat, x)
+        kl_divergence = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+        return reconstruction_loss + kl_divergence
 
 
 class Encoder(nn.Module):
@@ -20,7 +44,7 @@ class Encoder(nn.Module):
         
         self.selu = nn.SELU()
         
-        self.linear0 = nn.Linear((channels-26)*10, 435)
+        self.linear0 = nn.Linear((channels - 26) * 10, 435)
         self.mean_head = nn.Linear(435, output_dim)
         self.std_head = nn.Linear(435, output_dim)
     
@@ -57,5 +81,29 @@ class Decoder(nn.Module):
         z = z.contiguous().view(-1, z.size(-1))
         z = self.output_head(z)
         z = self.softmax(z)
-        z = z.contiguous().view(z.size(0), -1, z.size(-1))
+        z = z.contiguous().view(-1, self.output_dim, z.size(-1))
         return z
+
+
+if __name__ == '__main__':
+    # Test Encoder
+    encoder = Encoder()
+    x = torch.randn(5, 120, 35).softmax(dim=2)
+    output = encoder(x)
+    print(f"Encoder output shape: {output[0].shape}")
+    
+    # Test Decoder
+    decoder = Decoder()
+    z = torch.randn(5, 292)
+    output = decoder(z)
+    print(f"Decoder output shape: {output.shape}")
+    
+    # Test ChemicalVAE
+    model = ChemicalVAE()
+    output = model(x)
+    print(f"ChemicalVAE output shape: {output[0].shape}")
+    # print(output)
+    loss = model.loss_func(x, output[0], output[1], output[2])
+    # print(f"Loss: {loss}")
+    
+    
--- planning.md
+++ planning.md
@@ -1,3 +1,5 @@
 2023-11-01
 
-- [ ] Review the output shape of each layer in the ChemicalVAE model.
+- [ ] Refer to https://github.com/DeepGraphLearning/torchdrug/blob/master/torchdrug/core/core.py#L317 for config writting
+- [ ] Refer to https://github.com/cxhernandez/molencoder/blob/master/molencoder/models.py and https://github.com/topazape/molecular-VAE/blob/master/sample.py for featurizer
+- [x] Review the output shape of each layer in the ChemicalVAE model.
