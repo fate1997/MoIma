73c9beb31a1c58b9e358eca56b6f7dd15be14580
fate1997 <18719917821@163.com>
2024-01-16 15:04:23 +0000

    update experiments

--- experiment/vade.py
+++ experiment/vade.py
@@ -8,7 +8,7 @@ pkg_path = pathlib.Path(__file__).parent.parent
 if __name__ == '__main__':
     config = VaDEPipeConfig(raw_path=os.path.join(pkg_path, 'example/ilthermo_ionic_liquids.csv'),
                             processed_path=os.path.join(pkg_path, 'example/il.pt'),
-                            desc='IL_latent_dim256', 
+                            desc='IL_latent_dim128', 
                             save_processed=True,
                             force_reload=False,# `test` has passed
                             save_interval=10,
@@ -16,7 +16,7 @@ if __name__ == '__main__':
                             log_interval=5,
                             num_epochs=100,
                             batch_size=256,
-                            latent_dim=256,
+                            latent_dim=128,
                             n_clusters=50,
                             seq_len=256,
                             lr=1e-3,
--- experiment/vae.py
+++ experiment/vae.py
@@ -8,17 +8,17 @@ pkg_path = pathlib.Path(__file__).parent.parent
 if __name__ == '__main__':
     config = VAEPipeConfig(raw_path=os.path.join(pkg_path, 'example/ilthermo_ionic_liquids.csv'),
                             processed_path=os.path.join(pkg_path, 'example/il.pt'),
-                            desc='IL_latent_dim256', 
+                            desc='IL_latent_dim128_lr4', 
                             save_processed=True,
                             force_reload=False,# `test` has passed
                             save_interval=10,
                             in_step_mode=False,
                             log_interval=5,
                             num_epochs=100,
-                            batch_size=256,
-                            latent_dim=256,
+                            batch_size=128,
+                            latent_dim=128,
                             seq_len=256,
-                            lr=1e-3,
+                            lr=1e-4,
                             device='cuda:0',
                             vocab_size=55)
     pipe = VAEPipe(config)
